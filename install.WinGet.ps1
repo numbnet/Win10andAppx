e1234095e8ee9a53ccc6fd94b1e22ac87a372a72
numbnet <zusyurec@gmail.com>
2021-03-13 03:03:58 +0200

    Commit

--- install.WinGet.ps1
+++ install.WinGet.ps1
@@ -2,16 +2,13 @@
 
 mkdir C:\PS\
 
-##=======================================================
-##  Download Build and Release File
+##==============================================
+echo " Download Build and Release File anf install WinGet"
 
-##=======================================================
-##  install WinGet
+
 mkdir C:\PS\
 
-##=======================================================
-##  Download WinGet
-##-------------------
+##==========================================
 # Invoke-WebRequest -Uri "https://github.com/microsoft/winget-cli/releases/download/v0.1.4331-preview/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.appxbundle" -OutFile "C:\PS\WinGet.appxbundle"
 ##-------------------
 Invoke-WebRequest -Uri "https://github.com/microsoft/winget-cli/releases/download/v0.2.2941-preview/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.appxbundle" -OutFile "C:\PS\WinGet.appxbundle"
@@ -22,17 +19,17 @@ Add-AppxPackage "C:\PS\WinGet.appxbundle"
 
 ##=======================================================
 ##  install APPX 
-winget install "Microsoft.VisualStudioCode" --force
-winget install "Microsoft.WindowsTerminal" --silent
+#winget install "Microsoft.VisualStudioCode" --force
+#winget install "Microsoft.WindowsTerminal" --silent
 # winget install "Xampp" --silent
-winget install "CodeLite.CodeLite" --silent
-winget install "OpenJS.NodeJS" --silent
+#winget install "CodeLite.CodeLite" --silent
+#winget install "OpenJS.NodeJS" --silent
 
 ##=======================================================
 #### install tools
-New-Item -Path $env:SystemDrive\ -Name "NodeJS" -ItemType Directory -force;
+#New-Item -Path $env:SystemDrive\ -Name "NodeJS" -ItemType Directory -force;
 
-(New-Object System.Net.WebClient).DownloadFile('https://github.com/PowerShell/Win32-OpenSSH/releases/download/v8.1.0.0p1-Beta/OpenSSH-Win64.zip','$env:SystemDrive\NodeJS\OpenSSH-Win64.zip');
+#(New-Object System.Net.WebClient).DownloadFile('https://github.com/PowerShell/Win32-OpenSSH/releases/download/v8.1.0.0p1-Beta/OpenSSH-Win64.zip','$env:SystemDrive\NodeJS\OpenSSH-Win64.zip');
 
 
 ##=======================================================
@@ -48,10 +45,10 @@ winget install "Microsoft.PowerShell" --silent
 
 
 
-Find-Module Posh-SSH | Install-Module
-Import-Module Posh-SSH
-Install-Module -Name Posh-SSH
-iex (New-Object Net.WebClient).DownloadString("https://gist.github.com/darkoperator/6152630/raw/c67de4f7cd780ba367cccbc2593f38d18ce6df89/instposhsshdev")
+#Find-Module Posh-SSH | Install-Module
+#Import-Module Posh-SSH
+#Install-Module -Name Posh-SSH
+#iex (New-Object Net.WebClient).DownloadString("https://gist.github.com/darkoperator/6152630/raw/c67de4f7cd780ba367cccbc2593f38d18ce6df89/instposhsshdev")
 
 
 
